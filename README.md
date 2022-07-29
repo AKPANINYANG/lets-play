28b4326a66d14fbc3d5e74aa31744eae1abd3f6e
Emmanuel Akpaninyang vagrant <emmanuelakpaninyang@gmail.com>
2022-07-29 08:22:34 +0000

    added a README

---
+++ README.md
@@ -0,0 +1,1 @@
+This are series of projects to help practice my skills and refresh my knowledge on c
---
+++ fil.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+*main - Entry point
+*Return: always zero
+**/
+
+int main(void)
+{
+printf("Hello world, how're you guys doing\n");
+return (0);
+}
